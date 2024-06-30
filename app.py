f75b8f7888dd93d7358c8ef7a36a709638f92147
hardikk65 <hardikahuja.bt22cse@pec.edu.in>
2024-06-30 06:34:53 +0530

    Update app.py to fix database connection issue

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def index():
     
     return render_template('index.html',news_data = news_data)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
 
